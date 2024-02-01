fa957f47d1667ddcfe58133bfceca03b4889ffbf
toto-a <antoine.h2@hotmail.com>
2024-02-01 15:05:43 +0100

    Training file on CIFAR10

---
+++ ResNet/train.py
@@ -0,0 +1,144 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from tqdm import tqdm
+import gc
+
+from resnet import ResNet
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+num_classes = 10
+num_epochs = 20
+batch_size = 16
+learning_rate = 0.01
+
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+  
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    # define transforms
+    transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            normalize,
+    ])
+
+    if test:
+        dataset = datasets.CIFAR10(
+          root=data_dir, train=False,
+          download=True, transform=transform,
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+        return data_loader
+
+    # load the dataset
+    train_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(42)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler)
+ 
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler)
+
+    return (train_loader, valid_loader)
+
+
+# CIFAR10 dataset 
+train_loader, valid_loader = data_loader(data_dir='./data',
+                                         batch_size=64)
+
+test_loader = data_loader(data_dir='./data',
+                              batch_size=64,
+                              test=True)
+
+
+@torch.no_grad()
+def test_sample(model,loader):
+    correct=0
+    total=0
+    for images,label in loader :
+        images=images.to(device)
+        labels=labels.to(device)
+        outputs=model(images)
+        _,predicted=torch.max(outputs.data,1)
+        total+=labels.size(0)
+        correct+=(predicted==labels).sum().item()
+        del images,labels,ouputs
+    print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total)) 
+
+    
+    
+    
+def train(model,criterion,optimizer,loader):
+    for epoch in tqdm(range(num_epochs)):
+        for i, (images, labels) in enumerate(loader):  
+            # Move tensors to the configured device
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            del images, labels, outputs
+            torch.cuda.empty_cache()
+            gc.collect()
+
+
+        print ('Epoch [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, loss.item()))
+
+        test_sample(model,valid_loader)
+
+if __name__=="__main__":
+    
+   
+    model=ResNet([3,4,6,3],10).to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.001, momentum = 0.9)  
+
+    train(model,criterion,optimizer,train_loader)
+
