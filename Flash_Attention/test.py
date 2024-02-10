ec13d7c7dd2315115e75f7299a8d74bc33b807ad
toto-a <antoine.h2@hotmail.com>
2024-02-10 21:52:38 +0100

    Masking

--- Flash_Attention/test.py
+++ Flash_Attention/test.py
@@ -8,7 +8,7 @@ from normal_attention import classic_attention
 Q = torch.randn(1, 1, 2048, 512, requires_grad=True)
 K = torch.randn(1, 1, 2048, 512, requires_grad=True)
 V = torch.randn(1, 1, 2048, 512, requires_grad=True)
-mask = torch.randint(0, 2, (1, 2048))
+mask=torch.tril(torch.ones(2048, 2048).view(1, 1, 2048, 2048))
 
 def loss_fn(fn, *args):
     return torch.sum(fn(*args))
