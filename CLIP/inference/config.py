04093bb1ef7212367b8796f673f26ba38b6d2b62
toto-a <antoine.h2@hotmail.com>
2024-02-19 12:18:11 +0100

    Modiff in config

--- CLIP/inference/config.py
+++ CLIP/inference/config.py
@@ -2,10 +2,10 @@ import torch
 import os
 
 debug = True
-image_path = f"{os.getcwd}/inference/images"
-captions_path = f"{os.getcwd}/inference/captions"
+image_path = f"{os.getcwd()}/inference/data/Images"
+captions_path = f"{os.getcwd()}/inference/data/captions.txt"
 batch_size = 8
-num_workers = 0
+num_workers = 4
 lr = 1e-3
 weight_decay = 1e-3
 patience = 2
@@ -14,9 +14,9 @@ epochs = 5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_name = 'resnet50'
-image_embedding = 2048
+image_embeddings = 2048
 text_encoder_model = "distilbert-base-uncased"
-text_embedding = 768
+text_embeddings = 768
 text_tokenizer = "distilbert-base-uncased"
 max_length = 200
 
@@ -29,6 +29,6 @@ size = 224
 
 # for projection head; used for both image and text encoders
 num_projection_layers = 1
-projection_dim = 256 
+proj_dim = 256 
 dropout = 0.1
 
