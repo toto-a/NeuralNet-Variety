62109a265df1afb629cd6ffac00c0b9fd4ca5f95
toto-a <antoine.h2@hotmail.com>
2024-02-28 09:16:14 +0100

    start clip implementation

--- CLIP/clip.py
+++ CLIP/clip.py
@@ -4,4 +4,13 @@ from typing import List, Tuple, Union
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import timm
+import numpy as np
+
+
+
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
 
